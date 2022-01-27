87086c189dabe285a4342d9bb5769abf2d900909
Mahmudul2017 <mahmudulhasanshah@gmail.com>
2022-01-27 11:32:22 +0600

    Initial Commit

---
+++ OnlineShoppingApp-IOS/OnlineShoppingApp_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShoppingApp_IOSApp.swift
+//  OnlineShoppingApp-IOS
+//
+//  Created by Jaman on 27/1/22.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShoppingApp_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
