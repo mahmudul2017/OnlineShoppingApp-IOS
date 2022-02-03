52c524563e32156f3a764f4bf115ff5fcd3a50eb
Mahmudul2017 <mahmudulhasanshah@gmail.com>
2022-02-03 18:22:31 +0600

    + passthroughSubject implemented

--- OnlineShoppingApp-IOS/contact/ContactView.swift
+++ OnlineShoppingApp-IOS/contact/ContactView.swift
@@ -6,10 +6,34 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContactView: View {
+    var callPassThroughtSubject = CallPassThroughtSubject()
+    @State private var phoneNumber: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Phone No: \(phoneNumber ?? "Contact Us")")
+            
+            Button(action: {
+                self.callPassThroughtSubject.getPhoneNumber()
+            }, label: {
+                Text("Phone Number")
+            })
+        }
+        .onReceive(self.callPassThroughtSubject.callNumberChange, perform: { phoneNo in
+            self.phoneNumber = nil
+            self.phoneNumber = phoneNo
+        })
+    }
+}
+
+struct CallPassThroughtSubject {
+    var callNumberChange = PassthroughSubject<String, Never>()
+    
+    func getPhoneNumber() {
+        callNumberChange.send("01313-350895")
     }
 }
 
